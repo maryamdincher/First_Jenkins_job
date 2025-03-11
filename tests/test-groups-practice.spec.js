f0987b582682adb9dfaed8f9b3cd47d784b2ca66
maryamdincher <maryamdincher@gmail.com>
2025-03-11 09:29:20 -0600

    deleted time

--- tests/test-groups-practice.spec.js
+++ tests/test-groups-practice.spec.js
@@ -4,7 +4,6 @@ test.describe("Test Group", () => {
   // create beforeEach.
   test.beforeEach(async ({ page }) => {
     await page.goto("https://practice.cydeo.com/");
-    await page.waitForTimeout(3000);
   });
 
   test("Test A", async ({ page }) => {
